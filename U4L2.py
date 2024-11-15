0603d50e8fea346fc68ecbf5cef4da4330c8f277
Adam874796 <874796@pdsb.net>
2024-11-15 09:32:49 -0500

    Update U4L2.py

--- U4L2.py
+++ U4L2.py
@@ -1,28 +1,91 @@
 import turtle
 
 def modify(ln):
-  # Strips everything except the essential string
-  # ln = one line of data from first column to the carriage return
-  #
   mod_string = ""
-  badChars = ['"', ','] # chars to remove
-  ln = ln.strip() # clean out non-printable chars
+  badChars = ['"', ',']
+  ln = ln.strip()
   for c in ln:
     if c not in badChars:
       mod_string = mod_string + c
-  return mod_string # line of data should now be clean
+  return mod_string
 
-fh = open("smiley_emoji_mod.xpm", "r") # open a file handle
+def symbols(ln, file):
+  Rfile = []
+  mod_string = ""
+  badChars = ['"', ',', ' ']
+  ln = ln.strip()
+  for c in ln:
+    if c not in badChars:
+      mod_string = c
+      Rfile.append(mod_string)
+    elif c == ' ' and file == "teapot_mod.xpm":
+      mod_string = '~'
+      Rfile.append(mod_string)
+    elif c == ' ' and file == "smiley_emoji_mod.xpm":
+      mod_string = '\\'
+      Rfile.append(mod_string)
+    elif c == ' ' and file == "cool_smiley_mod.xpm":
+      mod_string = '~'
+      Rfile.append(mod_string)
+  return Rfile
+
+def writeDot(obj, rad, color):
+  obj.pendown()
+  obj.dot(rad, color)
+  obj.penup()
+ 
+usr = input("Enter 1 for smiley emoji, 2 for cool emoji, and 3 for teapot: ")
+usr = int(usr)
+if usr == 1:
+  f = "smiley_emoji_mod.xpm"
+if usr == 2:
+  f = "cool_smiley_mod.xpm"
+if usr == 3:
+  f = "teapot_mod.xpm"                                             
+ 
+fh = open(f, "r")
 color_data = fh.readline()
-color_data = int(modify(color_data))
+color_data = modify(color_data)
 [cols, rows, colors] = color_data.split()
 rows = int(rows)
 cols = int(cols)
 colors = int(colors)
-colorData = {} # a dictionary of symbols and colors
+
+colorData = {}
 
 for i in range(colors):
   cLine = fh.readline()
   cLine = modify(cLine)
   [sym, c, color] = cLine.split()
-  colorData[sym] = color # add a new dictionary entry
+  colorData[sym] = color
+
+t = turtle.Turtle()
+turtle.bgcolor("gray40")
+turtle.tracer(0, 0)
+t.hideturtle()
+
+t.penup()
+t.forward((-1*cols)//2)
+t.left(90)
+t.forward(rows//2)
+t.right(90)
+t.pendown()
+
+for y in range(rows):
+  fl = fh.readline()
+  xpm = symbols(fl, f)
+  for x in range(len(xpm)):
+    if xpm[x] in colorData:
+      writeDot(t, 4, colorData[xpm[x]])
+      t.forward(1)
+    else:
+      print(f"Warning: Undefined symbol '{xpm[x]}' at row {y}, col {x}")
+  t.forward(-1*(len(xpm)))
+  t.right(90)
+  t.forward(1)
+  t.left(90)
+print("The number of rows:", rows)
+print("The number of columns:", cols)
+print("The number of colors:", colors)
+for color in colorData.values():
+  print(color)
