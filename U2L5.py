82b9eaea49ef63f87a3b00f5af478df6585c54c2
Adam874796 <874796@pdsb.net>
2024-11-19 09:21:29 -0500

    Update U2L5.py

--- U2L5.py
+++ U2L5.py
@@ -74,27 +74,33 @@ num = input("choose a numerator: ")
 num = int(num)
 den = input("choose a denominator: ")
 den = int(den)
-f = Fraction2(num, den)
-print("your first fraction is", f)   
 
-num2 = input("choose a second numerator: ")
-num2 = int(num2)
-den2 = input("choose a second denominator: ")
-den2 = int(den2)
+if den == 0:
+    print("your denominator can not be 0")
+else:    
+    f = Fraction2(num, den)
+    print("your first fraction is", f)   
+    num2 = input("choose a second numerator: ")
+    num2 = int(num2)
+    den2 = input("choose a second denominator: ")
+    den2 = int(den2)
 
-choice = input("would you like to 'add' or 'multiply'? ")
-choice = str(choice)
-
-if choice == "multiply":
-    FMul = f.multiply(num2, den2)
-    print(FMul)
-    FReduce = f.reduce()
-    print("when reduced...", FReduce)
-elif choice == "add":
-    FAdd = f.add(num2, den2)
-    print(FAdd)
-    FReduce = f.reduce()
-    print("when reduced...", FReduce)
-else:
-    print("this is not an avalible option")
-# driver code
+    if den2 == 0:
+        print("your denominator can not be 0")
+    
+    if den and den2 != 0:
+        choice = input("would you like to 'add' or 'multiply'? ")
+        choice = str(choice)
+        
+        if choice == "multiply":
+            FMul = f.multiply(num2, den2)
+            print(FMul)
+            FReduce = f.reduce()
+            print("when reduced...", FReduce)
+        elif choice == "add":
+            FAdd = f.add(num2, den2)
+            print(FAdd)
+            FReduce = f.reduce()
+            print("when reduced...", FReduce)
+        else:
+            print("this is not an avalible option")
