229b5d2756bb2e83ad28e8979df70d68b09e02c5
Adam874796 <874796@pdsb.net>
2025-01-20 15:50:29 -0500

    Update CulminatingPolyDriver.py

--- CulminatingPolyDriver.py
+++ CulminatingPolyDriver.py
@@ -1,1 +1,11 @@
-#to be added
+"""
+Author : Adam Saad
+   Revison date : January 22nd 2025
+   Program : ICS4U0-1
+   Description : A code that solves the y-value of a given function when provided with an x-value
+   
+ General Variable Dictionary:
+ - 
+
+    
+"""
